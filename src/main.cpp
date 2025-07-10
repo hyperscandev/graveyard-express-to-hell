d7d7a11d8136df1f09e3b7cbd63bd8365df7b901
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2025-07-10 14:53:21 -0400

    Code now shows the player symbol
    Code has been revised to display the player symbol in place of the
    "Hello World" text.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@ This is the basic Hello World! example. It contains the very barebones code nece
 
 #include "tv/tv.h"
 
+// constants
+const unsigned short int SCREEN_WIDTH = 79;
+const unsigned short int SCREEN_HEIGHT = 29;
+
 // Stupid framebuffer for now
 unsigned short *fb = (unsigned short *) 0xA0400000;
 
@@ -27,10 +31,14 @@ int main(){
 	*/
 	tv_init(RESOLUTION_640_480, COLOR_RGB565, 0xA0400000, 0xA0400000, 0xA0400000);
 
+	// set the player's position
+	unsigned short int playerx = SCREEN_WIDTH / 2;
+	unsigned short int playery = SCREEN_HEIGHT / 2;
+
 	while(1){
 				
-		// Print text on TV
-		tv_print(fb, 28, 2, "Hello World!");
+		// draw the player on the screen
+		tv_print(fb, playerx, playery, "@");
 				
 	}
 		
