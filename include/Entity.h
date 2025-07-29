b58c10686848ff5d5b49c671b5781d962ee67b24
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2025-07-29 17:11:33 -0400

    Drafted the Entity class header
    Completed the initial draft of the header for the Entity class.

---
+++ include/Entity.h
@@ -0,0 +1,24 @@
+#ifndef ENTITY
+#define ENTITY
+
+class Entity {
+	public:
+		Entity(unsigned short x, unsigned short y, const char* c, unsigned short int color);
+		bool operator<(const Entity& other) const;
+		unsigned short int getId();
+		unsigned short int getX();
+		unsigned short int getY();
+		void setX(unsigned short int x);
+		void setY(unsigned short int y);
+		const char* getChar();
+		unsigned short int getColor();
+		void move(unsigned int dx, unsigned int dy);
+	private:
+		unsigned short int id;	// Instance-specific ID
+		static unsigned short int nextId;	// Static counter for auto-incrementing IDs
+		unsigned short int x;
+		unsigned short int y;
+		const char* c;
+		unsigned short int color;
+};
+#endif
