25dbb05edd261d5efa8868c377ecb41a454d7072
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2025-07-30 14:50:01 -0400

    The Entity class's initial version is complete
    Initial version of the Entity class has been successfully completed.

---
+++ src/Entity.cpp
@@ -0,0 +1,49 @@
+// include the header
+#include "../include/Entity.h"
+
+// holds the next id to assign
+unsigned short int Entity::nextId = 0;
+
+Entity::Entity(unsigned short x, unsigned short y, const char* c, unsigned short int color): id(++nextId) {
+	this->x = x;
+	this->y = y;
+	this->c = c;
+	this->color = color;
+}
+
+bool Entity::operator<(const Entity& other) const {
+        return this->id < other.id;
+}
+
+unsigned short int Entity::getId() {
+	return this->id;
+}
+
+unsigned short int Entity::getX() {
+	return this->x;
+}
+
+unsigned short int Entity::getY() {
+	return this->y;
+}
+
+const char* Entity::getChar() {
+	return this->c;
+}
+
+unsigned short int Entity::getColor() {
+	return this->color;
+}
+
+void Entity::setX(unsigned short int x) {
+	this->x = x;
+}
+
+void Entity::setY(unsigned short int y) {
+	this->y = y;
+}
+
+void Entity::move(unsigned int dx, unsigned int dy) {
+	this->x += dx;
+	this->y += dy;
+}
