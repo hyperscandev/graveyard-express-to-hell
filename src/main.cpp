eb54ee3619b48b31191b380edad5a14ae801a55e
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2025-08-08 12:29:38 -0400

    Updated main function to use the new Engine class
    Refined the main function to incorporate the new Engine class.

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,12 @@ This is the basic Hello World! example. It contains the very barebones code nece
 #include "tv/tv.h"
 #include "../include/Entity.h"
 #include "../include/InputHandlers.h"
+#include "../include/Engine.h"
 #include <set>
 
 // constants
 const unsigned short int SCREEN_WIDTH = 79;
 const unsigned short int SCREEN_HEIGHT = 29;
-
-// Stupid framebuffer for now
-unsigned short *fb = (unsigned short *) 0xA0400000;
 
 int main(){
 	
@@ -34,48 +32,24 @@ int main(){
 	*/
 	tv_init(RESOLUTION_640_480, COLOR_RGB565, 0xA0400000, 0xA0400000, 0xA0400000);
 
-	/*create the player entity */
-	Entity player = Entity(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, "@", 0);
-
 	// Set of entities
 	std::set<Entity> entities;
-
-	// add the player to the entities set
-	entities.insert(player);
-
-	// for storing the players previous position
-	unsigned short int previous_playerx;
-	unsigned short int previous_playery;
 
 	// create instance of event handler
 	EventHandler events = EventHandler();
 
+	/*create the player entity */
+	Entity player = Entity(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, "@", 0);
+
+	// create instance of the game engine
+	Engine engine = Engine(entities, events, player);
+
 	while(1){
+		// render the screen
+		engine.render();
+
 		// get events
-		Action* action = events.ev_input();
-
-		// action is a pointer to MovementAction (or derived from it)
-		if (dynamic_cast<MovementAction*>(action) != NULL) {
-			if (MovementAction* moveAction = dynamic_cast<MovementAction*>(action)) {
-				// save the players previous position
-				previous_playerx = player.getX();
-				previous_playery = player.getY();
-				
-				// set player's new position
-				player.setX(player.getX() + moveAction->getDX());
-				player.setY(player.getY() + moveAction->getDY());
-			}
-		}
-
-		// erase the player from the previous position
-		tv_print(fb, previous_playerx, previous_playery, " ");
-
-		// draw the player on the screen
-		tv_print(fb, player.getX(), player.getY(), player.getChar());
-		
-		// delete the pointer
-		delete action;
-				
+		engine.handle_events();
 	}
 		
 	return nExitCode;
