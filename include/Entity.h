cf638f1e2bb0671db6e727da4a70b35fed112bdb
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2025-09-04 15:06:48 -0400

    Added const-correctness to the entity class
    Implemented const-correctness in the entity class.

--- include/Entity.h
+++ include/Entity.h
@@ -4,18 +4,18 @@
 class Entity {
 	public:
 		Entity();
-		Entity(unsigned short x, unsigned short y, const char* c, unsigned short int color);
+		Entity(const unsigned short x, const unsigned short y, const char* c, const unsigned short int color);
 		bool operator<(const Entity& other) const;
-		unsigned short int getId();
-		unsigned short int getPrevX();
-		unsigned short int getPrevY();
-		unsigned short int getX();
-		unsigned short int getY();
-		void setX(unsigned short int x);
-		void setY(unsigned short int y);
-		const char* getChar();
-		unsigned short int getColor();
-		void move(unsigned int dx, unsigned int dy);
+		unsigned short int getId() const;
+		unsigned short int getPrevX() const;
+		unsigned short int getPrevY() const;
+		unsigned short int getX() const;
+		unsigned short int getY() const;
+		void setX(const unsigned short int x);
+		void setY(const unsigned short int y);
+		const char* getChar() const;
+		unsigned short int getColor() const;
+		void move(const unsigned int dx, const unsigned int dy);
 	private:
 		unsigned short int id;	// Instance-specific ID
 		static unsigned short int nextId;	// Static counter for auto-incrementing IDs
--- src/Entity.cpp
+++ src/Entity.cpp
@@ -8,7 +8,7 @@ unsigned short int Entity::nextId = 0;
 Entity::Entity() {
 }
 
-Entity::Entity(unsigned short x, unsigned short y, const char* c, unsigned short int color): id(++nextId) {
+Entity::Entity(const unsigned short x, const unsigned short y, const char* c, const unsigned short int color): id(++nextId) {
 	this->x = x;
 	this->y = y;
 	this->c = c;
@@ -19,43 +19,43 @@ bool Entity::operator<(const Entity& other) const {
         return this->id < other.id;
 }
 
-unsigned short int Entity::getId() {
+unsigned short int Entity::getId() const {
 	return this->id;
 }
 
-unsigned short int Entity::getPrevX() {
+unsigned short int Entity::getPrevX() const {
 	return this->prevX;
 }
 
-unsigned short int Entity::getPrevY() {
+unsigned short int Entity::getPrevY() const {
 	return this->prevY;
 }
 
-unsigned short int Entity::getX() {
+unsigned short int Entity::getX() const {
 	return this->x;
 }
 
-unsigned short int Entity::getY() {
+unsigned short int Entity::getY() const {
 	return this->y;
 }
 
-const char* Entity::getChar() {
+const char* Entity::getChar() const {
 	return this->c;
 }
 
-unsigned short int Entity::getColor() {
+unsigned short int Entity::getColor() const {
 	return this->color;
 }
 
-void Entity::setX(unsigned short int x) {
+void Entity::setX(const unsigned short int x) {
 	this->x = x;
 }
 
-void Entity::setY(unsigned short int y) {
+void Entity::setY(const unsigned short int y) {
 	this->y = y;
 }
 
-void Entity::move(unsigned int dx, unsigned int dy) {
+void Entity::move(const unsigned int dx, const unsigned int dy) {
 	this->prevX = this->x;
 	this->x += dx;
 	this->prevY = this->y;
