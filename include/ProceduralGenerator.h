142adfbacb6d88809f56b7c1e3c30f448025065a
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2025-10-23 17:11:41 -0400

    First version of dungeon generation function
    First implementation of the dungeon procedural generation function.

---
+++ include/ProceduralGenerator.h
@@ -0,0 +1,10 @@
+#ifndef PROCEDURALGENERATOR_H
+#define PROCEDURALGENERATOR_H
+
+// include required libraries
+#include "GameMap.h"
+#include "RectangularRoom.h"
+#include "Tile_Types.h"
+
+GameMap generate_dungeon(unsigned short int map_width, unsigned short int map_height);
+#endif
---
+++ src/ProceduralGenerator.cpp
@@ -0,0 +1,27 @@
+// include the header
+#include "../include/ProceduralGenerator.h"
+
+GameMap generateDungeon(unsigned short int map_width, unsigned short int map_height) {
+	GameMap dungeon = GameMap(map_width, map_height);
+
+	RectangularRoom room_1 = RectangularRoom(20, 15, 10, 15);
+	RectangularRoom room_2 = RectangularRoom(35, 15, 10, 15);
+
+	// --- Fill room_1.inner with floor tiles ---
+    std::pair<RectangularRoom::Slice, RectangularRoom::Slice> inner_1 = room_1.inner();
+    for (int x = inner_1.first.start; x < inner_1.first.end; ++x) {
+        for (int y = inner_1.second.start; y < inner_1.second.end; ++y) {
+            dungeon.set_tile(x, y, floor_tile);
+        }
+    }
+
+    // --- Fill room_2.inner with floor tiles ---
+    std::pair<RectangularRoom::Slice, RectangularRoom::Slice> inner_2 = room_2.inner();
+    for (int x = inner_2.first.start; x < inner_2.first.end; ++x) {
+        for (int y = inner_2.second.start; y < inner_2.second.end; ++y) {
+            dungeon.set_tile(x, y, floor_tile);
+        }
+    }
+
+	return dungeon;
+}
