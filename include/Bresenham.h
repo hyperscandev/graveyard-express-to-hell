fce4b200ab5b5395f193c324db8f28a5b0cb6700
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2025-11-06 14:16:17 -0500

    Initial header version for Bresenham function
    Preliminary header version for the Bresenham function.

---
+++ include/Bresenham.h
@@ -0,0 +1,11 @@
+#ifndef BRESENHAM_H
+#define BRESENHAM_H
+
+// include required libraries
+#include <vector>
+
+// type alias for readability
+typedef std::pair<int, int> Point;
+
+std::vector<Point> bresenham(int x1, int y1, int x2, int y2);
+#endif
