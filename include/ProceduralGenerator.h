d4f08e862b67bcee7b5b2fc5f460e59ac130c3ed
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2025-11-19 15:12:12 -0500

    Added header and prototype for `tunnel_between()`
    Incorporated the necessary header and function prototype for the
    tunnel_between() function.

--- include/ProceduralGenerator.h
+++ include/ProceduralGenerator.h
@@ -2,9 +2,13 @@
 #define PROCEDURALGENERATOR_H
 
 // include required libraries
+#include "Bresenham.h"
 #include "GameMap.h"
 #include "RectangularRoom.h"
 #include "Tile_Types.h"
 
+typedef std::pair<int, int> Point;
+
 GameMap generate_dungeon(unsigned short int map_width, unsigned short int map_height);
+std::vector<Point> tunnel_between(const Point &start, const Point &end);
 #endif
