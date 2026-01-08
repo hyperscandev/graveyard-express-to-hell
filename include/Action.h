3baef01ff6c54c1835e412bd78a3e7450e287a46
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-01-08 16:25:13 -0500

    Draft of action header file documentation
    Initial draft of the documentation for the action header file.

--- include/Action.h
+++ include/Action.h
@@ -1,7 +1,14 @@
 #ifndef ACTION_H
 #define ACTION_H
+
+/**
+ * Action class
+ *
+ * base class that handles the different possible actions
+ */
 class Action {
 	public:
+		//! pure virtual destructor
 		virtual ~Action() = 0;
 };
 #endif
