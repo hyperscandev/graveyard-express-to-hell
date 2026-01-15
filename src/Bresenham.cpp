ebc86074a607861fa425dc929396ef54f3e9ec91
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-01-15 17:52:07 -0500

    Draft for Bresenham's line algorithm documentation
    Initial draft of the documentation for the implementation of Bresenham's
    line algorithm.

--- src/Bresenham.cpp
+++ src/Bresenham.cpp
@@ -1,6 +1,19 @@
 // include the header
 #include "../include/Bresenham.h"
 
+/**
+ * Bresenham's Line Algorithm:
+ * Creates the most accurate representation of a straight line between two points (x1, y1) and (x2, y2) on a raster display.
+ * This method eliminates the need for costly floating-point calculations, resulting in a rapid and efficient process.
+ * The technique incrementally computes the next pixel to plot using an 'error' term, guiding the decision to move horizontally,
+ * vertically, or diagonally to remain true to the intended line.
+ *
+ * @param x1 x-coordinate of the start point 
+ * @param y1 y-coordinate of the start point
+ * @param x2 x-coordinate of the end point
+ * @param y2 y-coordinate of the end point
+ * @return Point vector with x and y coordinates
+ */
 std::vector<Point> bresenham(int x1, int y1, int x2, int y2) {
 	std::vector<Point> points;
     int dx = std::abs(x2 - x1);
