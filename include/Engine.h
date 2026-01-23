b0a37e8016fc7b96b1762868a1b85b5174bc1c77
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-01-23 17:48:21 -0500

    Initial documentation for the engine class header
    Preliminary documentation for the engine class header.

--- include/Engine.h
+++ include/Engine.h
@@ -8,16 +8,29 @@
 #include "../include/GameMap.h"
 #include <set>
 
+/**
+ * Game Engine class
+ *
+ * handles user input, rendering the game map, player, enemies and items
+ */
 class Engine {
 	public:
+		//! class constructor with parameters to hold set of entities, game map and player as well as an instance of the event handler class
 		Engine(std::set<Entity> entities, EventHandler event_handler, GameMap game_map, Entity player);
+		//! handles user input
 		void handle_events();
+		//! renders player, entities and game map to the framebuffer
 		void render();
 	private:
-		unsigned short *fb;				// Stupid framebuffer for now
-		std::set<Entity> entities;		// Set of entities
-		EventHandler event_handler;		// instance of event handler
-		GameMap game_map;				// instance of game map
-		Entity player;					// player entity
+		//! stupid framebuffer for now
+		unsigned short *fb;
+		//! set of entities
+		std::set<Entity> entities;
+		//! instance of event handler
+		EventHandler event_handler;
+		//! instance of game map
+		GameMap game_map;
+		//! player entity
+		Entity player;
 };
 #endif
