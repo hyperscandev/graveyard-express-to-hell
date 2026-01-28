ff3019eb1faabab9a2be1f7b0732e3c789f7121b
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-01-28 17:59:18 -0500

    Initial documentation for the input handler class
    Initial documentation for the header of the input handler class.

--- include/InputHandlers.h
+++ include/InputHandlers.h
@@ -5,9 +5,14 @@
 #include <cstddef>
 #include "../include/MovementAction.h"
 
+/**
+ * Handles events like getting user input
+ */
 class EventHandler {
 	public:
+		//! default class constructor
 		EventHandler();
+		//! gets user input
 		Action* ev_input();
 };
 #endif
