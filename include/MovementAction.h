0e940d4f9129a0cc2e5169a59edfb4e34991b3e4
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-01-30 17:58:54 -0500

    Draft documentation for Movement Action class
    Initial draft of the documentation for the header of the Movement Action
    class.

--- include/MovementAction.h
+++ include/MovementAction.h
@@ -3,13 +3,21 @@
 
 #include "Action.h"
 
+/**
+* class for handling the player's movement
+*/
 class MovementAction: public Action {
 	public:
+		//! class constructor with delta x and y velocities 
 		MovementAction(const unsigned short x, const unsigned short y);
+		//! returns the x delta
 		unsigned short int getDX() const;
+		//! returns the y delta
 		unsigned short int getDY() const;
 	private:
+		//! the x delta
 		unsigned short int dx;
+		//! the y delta
 		unsigned short int dy;
 };
 #endif
