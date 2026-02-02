7eca64b421c1e91ea3fc3512aeaeefaa1ef310d2
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-02-02 14:19:56 -0500

    Initial documentation for Movement Action class
    Preliminary version of the documentation for implementing the Movement
    Action class.

--- src/MovementAction.cpp
+++ src/MovementAction.cpp
@@ -1,15 +1,31 @@
 // include the header
 #include "../include/MovementAction.h"
 
+/**
+ * Class constructor with delta x and y parameters
+ *
+ * @param dx the initial delta x velocity
+ * @param dy the initial delta y velocity
+ */
 MovementAction::MovementAction(const unsigned short dx, const unsigned short dy): Action() {
 	this->dx = dx;
 	this->dy = dy;
 }
 
+/**
+ * Returns the delta x velocity
+ *
+ * @return delta x velocity
+ */
 unsigned short int MovementAction::getDX() const {
 	return this->dx;
 }
 
+/**
+ * Returns the delta y velocity
+ *
+ * @return delta y velocity
+ */
 unsigned short int MovementAction::getDY() const {
 	return this->dy;
 }
