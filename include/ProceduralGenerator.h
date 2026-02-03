dd57699941df7523901ea9016e1da67ac9322a11
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-02-03 17:17:09 -0500

    Initial documentation for the procedural generator
    Initial documentation for the header of the procedural generator.

--- include/ProceduralGenerator.h
+++ include/ProceduralGenerator.h
@@ -7,8 +7,11 @@
 #include "RectangularRoom.h"
 #include "Tile_Types.h"
 
+//! type definition for a single point
 typedef std::pair<int, int> Point;
 
+//! generates a dungeon
 GameMap generate_dungeon(unsigned short int map_width, unsigned short int map_height);
+//! generates a tunnel between two rooms
 std::vector<Point> tunnel_between(const Point &start, const Point &end);
 #endif
