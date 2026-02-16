b0831804ced8083951f742de8510ba0441986aa3
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-02-16 16:34:25 -0500

    Draft documentation for the RectangularRoom class
    Preliminary documentation for the header of the rectangular room class.

--- include/RectangularRoom.h
+++ include/RectangularRoom.h
@@ -4,23 +4,38 @@
 // include required libraries
 #include <utility>
 
+/**
+ * class for a handling a single room instance
+ */
 class RectangularRoom {
 
 	public:
+	//! struct for a slice of the room
 		struct Slice {
+			// start of the room slice exclusive
     		int start;
-    		int end; // exclusive
-			Slice(int start, int end) : start(start), end(end) {} // constructor
+    		// end of the room slice exclusive
+    		int end;
+    		//! constructor
+			Slice(int start, int end) : start(start), end(end) {}
 		};
 
+		//! class constructor with parameters for the starting position (x, y), width and height.
 		RectangularRoom(unsigned short int x, unsigned short int y, unsigned short int width, unsigned short int height);
+		//! returns the center point of the room
 		std::pair<int, int> center() const;
+		//! returns the inner area of the room
 		std::pair<Slice, Slice> inner() const;
+		//! returns if the room intersects another
 		bool intersects(RectangularRoom other) const;
 	private:
+		//! x starting point
 		unsigned short int x1;
+		//! y starting point
 		unsigned short int y1;
+		//! x ending point
 		unsigned short int x2;
+		//! y ending point
 		unsigned short int y2;
 };
 #endif
