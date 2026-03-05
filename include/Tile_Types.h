bfdce78d3e694c1040448ebd1a4662380f3eb577
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-03-05 17:50:23 -0500

    Documentation for the tile type header
    Preliminary documentation for tile type structs, helper functions, and
    definitions header.

--- include/Tile_Types.h
+++ include/Tile_Types.h
@@ -4,21 +4,31 @@
 // include required libraries
 #include "tv/tv.h"
 
+//! struct for a single graphic
 struct Graphic {
+	// character associated with the graphic
 	unsigned char ch;
+	// foreground color
 	unsigned short fg;
+	// background color
 	unsigned short bg;
 };
 
+//! struct for a single tile
 struct Tile {
+	// is tile is walkable?
 	bool walkable;
+	// is tile is transparent?
 	bool transparent;
+	// dark graphic of the tile
 	Graphic dark;
 };
 
-// helper functiom
+//! helper function for defining a new tile
 Tile new_tile(const bool walkable, const bool transparent, const unsigned char ch, const unsigned char fg_r, const unsigned char fg_g, const unsigned char fg_b, const unsigned char bg_r, const unsigned char bg_g, const unsigned char bg_b);
 
+//! floor tile definition is defined in source
 extern Tile floor_tile;
+//! floor wall definition is defined in source
 extern Tile wall_tile;
 #endif
