5f1fa16ffa77ddf0ffab0c63c83f701a14ed1eba
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-03-11 17:58:29 -0400

    Documentation for the tile helper function
    Initial documentation outlining the specifications for floor and wall
    tiles, alongside the implementation of the tile helper function.

--- src/Tile_Types.cpp
+++ src/Tile_Types.cpp
@@ -1,6 +1,21 @@
 // include the header
 #include "../include/Tile_Types.h"
 
+/**
+ * Helper function for creating new Tile definitions
+ *
+ * @param is the tile walkable
+ * @param is the tile transparent
+ * @param ch character for the tile
+ * @param fg_r intensitiy of red primary for foreground
+ * @param fg_g intensitiy of green primary for foreground
+ * @param fg_b intensitiy of blue primary for foreground
+ * @param bg_r intensitiy of red primary for background
+ * @param bg_g intensitiy of green primary for background
+ * @param bg_b intensitiy of blue primary for background
+ *
+ * @return new Tile struct
+ */
 Tile new_tile(const bool walkable, const bool transparent, const unsigned char ch, const unsigned char fg_r, const unsigned char fg_g, const unsigned char fg_b, const unsigned char bg_r, const unsigned char bg_g, const unsigned char bg_b) {
 	Tile tile;
     tile.walkable = walkable;
@@ -11,6 +26,7 @@ Tile new_tile(const bool walkable, const bool transparent, const unsigned char c
     return tile;
 }
 
+//! floor tile definition
 Tile floor_tile = new_tile(
     true, // walkable
     true, // transparent
@@ -19,6 +35,7 @@ Tile floor_tile = new_tile(
     0, 0, 0 // bg black
 );
 
+//! wall tile definition
 Tile wall_tile = new_tile(
     false, // walkable
     false, // transparent
