69163ab5f35868d24084aa1b0cc70ace436325c7
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-05-26 17:05:28 -0400

    Header for GameMap class ensures const correctness
    Header for the GameMap class has been updated to incorporate const
    correctness in its member functions.

--- include/GameMap.h
+++ include/GameMap.h
@@ -15,19 +15,19 @@ class GameMap {
 		//! class constructor
 		GameMap();
 		//! class constructor with parameters to define it's width and height
-		GameMap(unsigned int width, unsigned int height);
+		GameMap(const unsigned int width, const unsigned int height);
 		//! returns the maps width
 		unsigned int getWidth() const;
 		//! returns the maps height
 		unsigned int getHeight() const;
 		//! sets a tile at a given x/y coordinate
-		void set_tile(unsigned short int x, unsigned short int y, const Tile& tile);
+		void set_tile(const unsigned short int x, const unsigned short int y, const Tile& tile);
 		//! returns if the given coordinate is inbounds
-		bool in_bounds(unsigned short int x, unsigned short int y) const;
+		bool in_bounds(const unsigned short int x, const unsigned short int y) const;
 		//! returns if the tile at the given x/y coordinate is walkable
-		bool is_walkable(unsigned short int x, unsigned short int y);
+		bool is_walkable(const unsigned short int x, const unsigned short int y) const;
 		//! renders the game map
-		void render(unsigned short *fb);
+		void render(unsigned short *fb) const;
 	private:
 		//! the maps width
 		unsigned int width;
