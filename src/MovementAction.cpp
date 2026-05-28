0054d2d3171f05f8b9a818a0397db9b5bf8a1778
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-05-28 11:50:46 -0400

    Member function added to the MovementAction class
    Perform member function has been implemented in the MovementAction
    class.

--- src/MovementAction.cpp
+++ src/MovementAction.cpp
@@ -29,3 +29,24 @@ unsigned short int MovementAction::getDX() const {
 unsigned short int MovementAction::getDY() const {
 	return this->dy;
 }
+
+/**
+ * Move's an entity based on it's x and y delta velocity
+ *
+ * @param engine instance of the game engine
+ * @param entity the entity to move
+ */
+void MovementAction::perform(Engine engine, Entity entity) {
+	unsigned short int dest_x = entity.getX() + this->dx;
+	unsigned short int dest_y = entity.getY() + this->dy;
+
+	if (engine.getGameMapInstance().in_bounds(dest_x, dest_y)) {
+		return;
+	}
+
+	if (engine.getGameMapInstance().is_walkable(dest_x, dest_y)) {
+		return;
+	}
+
+	entity.move(this->dx, this->dy);
+}
