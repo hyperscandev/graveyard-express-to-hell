9e21b02c0fd83303c7b8d8d2deb12c5b52061d05
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-06-09 13:57:43 -0400

    Perform function is updated to fit the new changes
    Implementation of the perform function has been modified to align with
    the new changes.

--- src/MovementAction.cpp
+++ src/MovementAction.cpp
@@ -36,15 +36,15 @@ unsigned short int MovementAction::getDY() const {
  * @param engine instance of the game engine
  * @param entity the entity to move
  */
-void MovementAction::perform(Engine engine, Entity entity) {
+void MovementAction::perform(Engine engine, Entity& entity) {
 	unsigned short int dest_x = entity.getX() + this->dx;
 	unsigned short int dest_y = entity.getY() + this->dy;
 
-	if (engine.getGameMapInstance().in_bounds(dest_x, dest_y)) {
+	if (!engine.getGameMapInstance().in_bounds(dest_x, dest_y)) {
 		return;
 	}
 
-	if (engine.getGameMapInstance().is_walkable(dest_x, dest_y)) {
+	if (!engine.getGameMapInstance().is_walkable(dest_x, dest_y)) {
 		return;
 	}
 
