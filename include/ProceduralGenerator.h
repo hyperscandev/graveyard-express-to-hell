afec2a8ff08986d5ee73d3cc0a144e8df723b0ed
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-08-11 17:40:19 -0400

    Applied const correctness in the new method
    Applied const correctness to the header of the new method for dungeon
    generation.

--- include/ProceduralGenerator.h
+++ include/ProceduralGenerator.h
@@ -13,7 +13,7 @@ typedef std::pair<int, int> Point;
 
 //! generates a dungeon
 GameMap generate_dungeon(unsigned short int map_width, unsigned short int map_height);
-GameMap generate_dungeon(unsigned short int max_rooms, unsigned short int room_min_size, unsigned short int room_max_size, unsigned short int map_width, unsigned short int map_height, Entity &player);
+const GameMap generate_dungeon(const unsigned short int max_rooms, const unsigned short int room_min_size, const unsigned short int room_max_size, const unsigned short int map_width, const unsigned short int map_height, Entity &player);
 //! generates a tunnel between two rooms
 std::vector<Point> tunnel_between(const Point &start, const Point &end);
 #endif
