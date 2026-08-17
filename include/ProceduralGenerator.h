5f6f2509fe4f56c5e8f4a5413df3ce4063d28d83
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-08-17 17:03:55 -0400

    Removed the short-term dungeon creation function
    Removed the short-term version of the dungeon creation function.

--- include/ProceduralGenerator.h
+++ include/ProceduralGenerator.h
@@ -12,7 +12,6 @@
 typedef std::pair<int, int> Point;
 
 //! generates a dungeon
-GameMap generate_dungeon(unsigned short int map_width, unsigned short int map_height);
 const GameMap generate_dungeon(const unsigned short int max_rooms, const unsigned short int room_min_size, const unsigned short int room_max_size, const unsigned short int map_width, const unsigned short int map_height, Entity &player);
 //! generates a tunnel between two rooms
 std::vector<Point> tunnel_between(const Point &start, const Point &end);
--- src/ProceduralGenerator.cpp
+++ src/ProceduralGenerator.cpp
@@ -4,47 +4,6 @@
 /**
  * Generates a dungeon of a given width and height
  *
- * @param map_width the width of the game map the dungeon will reside
- * @param map_height the height of the game map the dungeon will reside
- *
- * @return game map instance containing the dungeon
- */
-GameMap generate_dungeon(unsigned short int map_width, unsigned short int map_height) {
-	GameMap dungeon = GameMap(map_width, map_height);
-
-	RectangularRoom room_1 = RectangularRoom(20, 5, 5, 7);
-	RectangularRoom room_2 = RectangularRoom(35, 5, 5, 7);
-
-	// --- Fill room_1.inner with floor tiles ---
-    std::pair<RectangularRoom::Slice, RectangularRoom::Slice> inner_1 = room_1.inner();
-    for (int x = inner_1.first.start; x < inner_1.first.end; ++x) {
-        for (int y = inner_1.second.start; y < inner_1.second.end; ++y) {
-            dungeon.set_tile(x, y, floor_tile);
-        }
-    }
-
-    // --- Fill room_2.inner with floor tiles ---
-    std::pair<RectangularRoom::Slice, RectangularRoom::Slice> inner_2 = room_2.inner();
-    for (int x = inner_2.first.start; x < inner_2.first.end; ++x) {
-        for (int y = inner_2.second.start; y < inner_2.second.end; ++y) {
-            dungeon.set_tile(x, y, floor_tile);
-        }
-    }
-    
-	std::vector<Point> path = tunnel_between(room_2.center(), room_1.center());
-
-	for (std::vector<Point>::const_iterator it = path.begin(); it != path.end(); ++it) {
-    	int x = it->first;
-    	int y = it->second;
-    	dungeon.set_tile(x, y, floor_tile);
-	}
-
-	return dungeon;
-}
-
-/**
- * Generates a dungeon of a given width and height
- *
  * @param max_room the maxinum number of rooms to attempt to generate
  * @param room_min_size mininum size of the generated room(characters)
  * @param room_max_size maxinum size of the generated room(characters)
