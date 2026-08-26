25e2727ed0a4a13c373ba43d81eba7cf154e9798
hyperscandev <142263206+hyperscandev@users.noreply.github.com>
2026-08-26 17:22:08 -0400

    Defined graphics for unexplored and unseen tiles
    A graphic definition for shroud has been added for tiles that are
    unexplored and unseen.

--- include/Tile_Types.h
+++ include/Tile_Types.h
@@ -29,6 +29,9 @@ struct Tile {
 //! helper function for defining a new tile
 Tile new_tile(const bool walkable, const bool transparent, const unsigned char ch, const unsigned char dark_fg_r, const unsigned char dark_fg_g, const unsigned char dark_fg_b, const unsigned char dark_bg_r, const unsigned char dark_bg_g, const unsigned char dark_bg_b, const unsigned char light_fg_r, const unsigned char light_fg_g, const unsigned char light_fg_b, const unsigned char light_bg_r, const unsigned char light_bg_g, const unsigned char light_bg_b);
 
+//! SHROUD represents unexplored, unseen tiles
+const Graphic SHROUD = {' ', RGB565(255, 255, 255), RGB565(0, 0, 0)};
+
 //! floor tile definition is defined in source
 extern Tile floor_tile;
 //! floor wall definition is defined in source
